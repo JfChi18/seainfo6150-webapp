af0969f05d8a170faf88ecb5b43945c417094df8
choco.lette <gretchenchi@gmail.com>
2021-03-21 22:15:12 -0700

    Update DynamicArticle.jsx

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -1,20 +1,30 @@
 import React from "react";
-import HTMLText from "../HTMLText/HTMLText";
+import HTMLText from "../HTMLText/HTMLText.jsx";
+import { Link } from "react-router-dom";
+import styles from "./DynamicArticle.module.css";
 
 const DynamicArticle = (props) => {
   return (
-    <article>
-      <header>
-        <h1>{props.article.title}</h1>
-        <address>
-          {props.article.author} (
-          <a href="mailto:kate.farley@nytimes.com">{props.article.authorEmail}</a>)
-          <br />
-        </address>
-        <time datetime={props.article.timeStamp}>{props.article.displayDate}</time>
-      </header>
-      <HTMLText text={props.article.text} />
-    </article>
+    <div>
+      <Link to="/articlelist" className={styles.backLink}>Back</Link>
+      <article className={styles.article}>
+        <header>
+          <h1>{props.article.title}</h1>
+          <address>
+            by {props.article.author} (
+            <a href={`mailto:${props.article.authorEmail}`}>
+              {props.article.authorEmail}
+            </a>
+            )
+            <br />
+          </address>
+          <time dateTime={props.article.timeStamp}>
+            {props.article.displayDate}
+          </time>
+        </header>
+        <HTMLText text={props.article.text} />
+      </article>
+    </div>
   );
 };
 
