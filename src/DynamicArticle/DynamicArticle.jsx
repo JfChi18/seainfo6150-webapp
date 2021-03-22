4bad24fecfd39d0c0e103024f0a559aacbd5ccc6
choco.lette <gretchenchi@gmail.com>
2021-03-21 22:48:23 -0700

    CSS Assignment

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -1,30 +1,24 @@
 import React from "react";
-import HTMLText from "../HTMLText/HTMLText.jsx";
-import { Link } from "react-router-dom";
-import styles from "./DynamicArticle.module.css";
+import HTMLText from "../HTMLText/HTMLText";
+import {Link} from "react-router-dom";
+import styles from './DynamicArticle.module.css';
 
-const DynamicArticle = (props) => {
+
+const DynamicArticle = ({article}) => {
   return (
-    <div>
-      <Link to="/articlelist" className={styles.backLink}>Back</Link>
-      <article className={styles.article}>
-        <header>
-          <h1>{props.article.title}</h1>
-          <address>
-            by {props.article.author} (
-            <a href={`mailto:${props.article.authorEmail}`}>
-              {props.article.authorEmail}
-            </a>
-            )
-            <br />
-          </address>
-          <time dateTime={props.article.timeStamp}>
-            {props.article.displayDate}
-          </time>
-        </header>
-        <HTMLText text={props.article.text} />
-      </article>
-    </div>
+    <article>
+        <Link to="/articlelist"><span className={styles.linkButton}>Back</span></Link>
+        <div className={styles.articleContainer}>
+        <h1>{article.title}</h1>
+        <address>{article.author} (
+          <a href={"mailto:" + article.authorEmail}>{article.authorEmail}</a>)
+          <br />
+        </address>
+        <time dateTime={article.timeStamp}>{article.displayDate}</time>
+        <br />
+      <HTMLText text={article.text} />
+      </div> 
+    </article>
   );
 };
 
