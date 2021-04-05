01dd54995e45440cf400e86c113d4eb11fff57ca
jiafeng <jiafeng@jiafengs-MacBook-Pro.local>
2021-04-04 22:06:21 -0700

    [responsive-layout]

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import PropTypes from "prop-types";
 import ArticleListItem from "../ArticleListItem/ArticleListItem.jsx";
--- src/ArticleListItem/ArticleListItem.jsx
+++ src/ArticleListItem/ArticleListItem.jsx
@@ -1,38 +1,42 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import styles from "./ArticleListItem.module.css";
 import ArticleTextToggleButton from "../ArticleTextToggleButton/ArticleTextToggleButton.jsx";
-import {Link} from "react-router-dom";
-import styles from './ArticleListItem.module.css';
-import ArticleImage from "../ArticleImage/ArticleImage.jsx";
+import ArticleImage from "../ArticleImage/ArticleImage";
 
+const ArticleListItem = (props) => {
+  const [isTextShowing, setIsTextShowing] = useState(false);
 
-const ArticleListItem = ({article}) => {
-  const [showDetails, setShowDetails] = useState(false);
-
-  let buttonText = 'Show more';
-
-  if (showDetails) {
-    buttonText = 'Show less';
+  function onClick() {
+    setIsTextShowing(!isTextShowing);
   }
 
-  const onClick = () => {
-    setShowDetails(!showDetails);
-  };
-
   return (
-    <article className={styles.card}>
-      <ArticleImage url={article.image._url} title={article.title}/>
-      <Link to={'/articlelist/'+ article.slug}>
-        <h3 className={styles.articleListItem}>{article.title}</h3>
-      </Link>
-      {showDetails && (
-        <Fragment>
-          <b><time dateTime={article.timeStamp}>{article.displayDate}</time></b>
-          <p>{article.shortText}</p>
-        </Fragment>
-      )}
-      <ArticleTextToggleButton buttonText={buttonText} onClick={onClick}/>
-    </article>
+    <li className={styles.container}>
+      <article className={styles.article}>
+        <div className={styles.text}>
+        <ArticleImage url={props.article.image._url} title={props.article.title}/>
+          <h2 className={styles.title}>{props.article.title}</h2>
+          {isTextShowing && (
+            <div className={styles.text}>
+              <p>{props.article.shortText}</p>
+              <time className={styles.time} dateTime={props.article.timeStamp}>
+                {props.article.displayDate}
+              </time>
+            </div>
+          )}
+        </div>
+        <ArticleTextToggleButton
+          buttonText={isTextShowing ? "Show less" : "Show more"}
+          onClick={onClick}
+        />
+      </article>
+    </li>
   );
 };
 
+ArticleListItem.propTypes = {
+  article: PropTypes.object.isRequired,
+};
 export default ArticleListItem;
